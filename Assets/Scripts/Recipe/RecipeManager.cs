8e26ab891ca523964670337ab48509b46d03084a
Loctryl <loctryl.207@gmail.com>
2024-03-04 15:02:56 +0100

    <ADD> Debut of recipes feature

---
+++ Assets/Scripts/Recipe/RecipeManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecipeManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _ingredients;
+    
+    private List<List<int>> _recipes;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _recipes = new List<List<int>>();
+        
+        CreateRecipe();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateRecipe()
+    {
+        List<int> recipe = new List<int>();
+        
+        recipe.Add(0);
+
+        for (int i = 0; i < Random.Range(2, 5); i++)
+        {
+            recipe.Add(Random.Range(1, _ingredients.Count-1));
+        }
+        
+        recipe.Add(0);
+        
+        _recipes.Add(recipe);
+    }
+}
