af495b7e351781a271e2217fb44690a434a6209b
Loctryl <loctryl.207@gmail.com>
2024-03-06 11:41:31 +0100

    <ADD> Building burgers

---
+++ Assets/Scripts/SocketEvents.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SocketEvents : MonoBehaviour
+{
+	[SerializeField] private XRSocketInteractor socketInteractor;
+
+	private void Start()
+	{
+		socketInteractor.selectEntered.AddListener(UsedSocket);
+		socketInteractor.selectExited.AddListener(UnUsedSocket);
+	}
+
+	private void OnDestroy()
+	{
+		socketInteractor.selectEntered.RemoveListener(UsedSocket);
+		socketInteractor.selectExited.RemoveListener(UnUsedSocket);
+	}
+
+	void UsedSocket(SelectEnterEventArgs args)
+	{
+		args.interactableObject.transform.GetComponent<ISocketable>().SocketOn();
+	}
+	
+	void UnUsedSocket(SelectExitEventArgs args)
+	{
+		args.interactableObject.transform.GetComponent<ISocketable>().SocketOff();
+	}
+}
